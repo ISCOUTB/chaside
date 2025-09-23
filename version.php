b44a90a81aec99d4f0a488c113da41c725e18ac7
Server Dev <server@saviodev.utb.edu.co>
2025-09-23 04:28:08 +0000

    Agregar soporte multiidioma completo: CHASIDE v1.0.2
    - Creado archivo de traducción al español (lang/es/block_chaside.php)
    - Todas las 98 preguntas del test CHASIDE traducidas al español
    - Soporte completo para interfaz bilingüe (español/inglés)
    - Las preguntas se muestran según el idioma configurado en Moodle
    - Incrementada versión a 1.0.2 (2025092301)

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_chaside';
-$plugin->version = 2025092300;
+$plugin->version = 2025092301;
 $plugin->requires = 2022041900;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.0.1';
+$plugin->release = '1.0.2';
